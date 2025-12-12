8434f75c0951962d29ed2aac961203d3ac1bca88
antony-akshay <akshay.dev.ac.in@gmail.com>
2025-12-12 17:52:22 +0530

    test

--- anchor/tests/basic.test.ts
+++ anchor/tests/basic.test.ts
@@ -1,16 +1,44 @@
 import * as anchor from '@coral-xyz/anchor'
 import { Program } from '@coral-xyz/anchor'
 import { Basic } from '../target/types/basic'
-
+import { PublicKey } from '@solana/web3.js'
 describe('basic', () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env())
 
+  const provider = anchor.AnchorProvider.env()
   const program = anchor.workspace.Basic as Program<Basic>
 
   it('should run the program', async () => {
-    // Add your test here.
-    const tx = await program.methods.greet().rpc()
+    // // Add your test here.
+    // const tx = await program.methods.greet().rpc()
+    // console.log('Your transaction signature', tx)
+
+    const add = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("ENTRY_STATE"),
+        Buffer.from("test"),
+        provider.wallet.publicKey.toBuffer()
+      ],
+      program.programId
+    )[0]
+
+    const tx = await program.methods.createEntry(
+      "test",
+      "test",
+      new anchor.BN(Math.floor(Date.now() / 1000)),
+      ""
+    )
+      .accounts({
+        journalAccount: add,
+        signer: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc()
     console.log('Your transaction signature', tx)
+
+    const acc = await program.account.journalEntry.fetch(add)
+    console.log(acc)
+    
   })
 })
